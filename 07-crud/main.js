0dc1fb1dfc258d5c32cdee0a04cfc557204356d3
RaphaelSM <raphaelnsx@gmail.com>
2021-12-20 23:21:54 -0300

    inicio do crud
    inicio do crud.

--- 07-crud/main.js
+++ 07-crud/main.js
@@ -6,6 +6,19 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: 'Raphael',
+    email: 'raphael@gmail.com',
+    celular: '47256893321',
+    cidade: 'Blumenau'
+}
+
+//CRUD
+const createClient = (client) => {
+    localStorage.setItem('db_client', client) //11:47
+}
+
+//Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
