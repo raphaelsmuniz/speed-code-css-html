fd52ff79cbc73a4dfa906068c0e8b1d62ce2ab04
RaphaelSM <raphaelnsx@gmail.com>
2021-12-24 16:31:34 -0300

    alteração-241221

--- 07-crud/main.js
+++ 07-crud/main.js
@@ -7,15 +7,54 @@ const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
 const tempClient = {
-    nome: 'Raphael',
-    email: 'raphael@gmail.com',
+    nome: 'Nicolas',
+    email: 'raphael-muniz@gmail.com',
     celular: '47256893321',
     cidade: 'Blumenau'
 }
 
-//CRUD
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (db_client) => localStorage.setItem('db_client', JSON.stringify(db_client))
+
+//CRUD - DELETE
+const deleteClient = (index) => {
+    const db_client = readClient()
+    db_client.splice(index, 1)
+    setLocalStorage(db_client)
+}
+
+//CRUD - UPDATE
+const updateClient = (index, client) => {
+    const db_client = readClient()
+    db_client[index] = client
+    setLocalStorage(db_client)
+}
+
+//CRUD - READ
+const readClient = () => getLocalStorage()
+
+//CRUD - create
 const createClient = (client) => {
-    localStorage.setItem('db_client', client) //11:47
+    const db_client = getLocalStorage()
+    db_client.push(client)
+    setLocalStorage(db_client)
+}
+
+const isValidFields = () => {
+    document.getElementById('form').reportValidity()
+}
+
+//Interação com layout
+const saveClient = () => {
+    if (isValidFields()) {
+        const client = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value
+        }
+        createClient(client)
+    }
 }
 
 //Eventos
@@ -24,3 +63,6 @@ document.getElementById('cadastrarCliente')
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
